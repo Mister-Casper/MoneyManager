0e997f78141056eebc784bdb27d8f38ed57925b2
UserName <44705397+Mister-Casper@users.noreply.github.com>
2022-02-08 13:26:58 +0700

    Add transaction category selector

---
+++ app/src/main/java/com/sgcdeveloper/moneymanager/domain/util/TransactionCategory.kt
@@ -0,0 +1,69 @@
+package com.sgcdeveloper.moneymanager.domain.util
+
+import androidx.compose.ui.graphics.toArgb
+import com.sgcdeveloper.moneymanager.R
+import com.sgcdeveloper.moneymanager.presentation.theme.*
+
+sealed class TransactionCategory (){
+    abstract val color:Int
+    abstract val icon:Int
+    abstract val description:Int
+
+    sealed class ExpenseCategory(override val color: Int, override val icon: Int, override val description: Int) :
+        TransactionCategory() {
+        object Bills : ExpenseCategory(wallet_color_1.toArgb(), R.drawable.bills_icon, R.string.bills)
+        object Clothes : ExpenseCategory(wallet_color_2.toArgb(), R.drawable.clothes_icon, R.string.clothes)
+        object Education : ExpenseCategory(wallet_color_3.toArgb(), R.drawable.education_icon, R.string.education)
+        object Entertainment :
+            ExpenseCategory(wallet_color_4.toArgb(), R.drawable.entertaiment_icon, R.string.entertainment)
+
+        object Sport : ExpenseCategory(wallet_color_5.toArgb(), R.drawable.sport_icon, R.string.sport)
+        object Food : ExpenseCategory(wallet_color_6.toArgb(), R.drawable.food_icon, R.string.food)
+        object Gifts : ExpenseCategory(wallet_color_7.toArgb(), R.drawable.gift_icon, R.string.gifts)
+        object Health : ExpenseCategory(wallet_color_8.toArgb(), R.drawable.health_icon, R.string.health)
+        object Furniture : ExpenseCategory(wallet_color_9.toArgb(), R.drawable.furniture_icon, R.string.furniture)
+        object Pet : ExpenseCategory(wallet_color_10.toArgb(), R.drawable.pet_icon, R.string.pet)
+        object Shopping : ExpenseCategory(wallet_color_11.toArgb(), R.drawable.shopping_icon, R.string.shopping)
+        object Transport : ExpenseCategory(wallet_color_12.toArgb(), R.drawable.transport_icon, R.string.transport)
+        object Travel : ExpenseCategory(wallet_color_13.toArgb(), R.drawable.travel_icon, R.string.travel)
+        object Others : ExpenseCategory(wallet_color_14.toArgb(), R.drawable.others_icon, R.string.others)
+
+        companion object {
+            fun getItems(): List<ExpenseCategory> {
+                return listOf(
+                    Bills,
+                    Clothes,
+                    Education,
+                    Entertainment,
+                    Sport,
+                    Food,
+                    Gifts,
+                    Health,
+                    Furniture,
+                    Pet,
+                    Shopping,
+                    Transport,
+                    Travel,
+                    Others
+                )
+            }
+        }
+    }
+
+    sealed class IncomeCategory(override val color: Int, override val icon: Int, override val description: Int) : TransactionCategory() {
+        object Award : IncomeCategory(wallet_color_1.toArgb(), R.drawable.sport_icon, R.string.award)
+        object Bonus : IncomeCategory(wallet_color_2.toArgb(), R.drawable.bonus_icon, R.string.bonus)
+        object Dividend : IncomeCategory(wallet_color_3.toArgb(), R.drawable.dividend_icon, R.string.dividend)
+        object Investment : IncomeCategory(wallet_color_4.toArgb(), R.drawable.investment_icon, R.string.investment)
+        object Lottery : IncomeCategory(wallet_color_5.toArgb(), R.drawable.lottery_icon, R.string.lottery)
+        object Salary : IncomeCategory(wallet_color_6.toArgb(), R.drawable.salary_icon, R.string.salary)
+        object Tips : IncomeCategory(wallet_color_7.toArgb(), R.drawable.tips_icon, R.string.tips)
+        object Others : IncomeCategory(wallet_color_8.toArgb(), R.drawable.others_icon, R.string.others)
+
+        companion object {
+            fun getItems(): List<IncomeCategory> {
+                return listOf(Award, Bonus, Dividend, Investment, Lottery, Salary, Tips, Others)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/sgcdeveloper/moneymanager/presentation/ui/dialogs/SelectTransactionCategoryDialog.kt
@@ -0,0 +1,147 @@
+package com.sgcdeveloper.moneymanager.presentation.ui.dialogs
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.sgcdeveloper.moneymanager.R
+import com.sgcdeveloper.moneymanager.domain.util.TransactionCategory
+import com.sgcdeveloper.moneymanager.presentation.theme.blue
+import com.sgcdeveloper.moneymanager.presentation.theme.white
+
+@Composable
+fun SelectTransactionCategoryDialog(
+    isIncome: Boolean,
+    defaultCategory: TransactionCategory? = null,
+    onAdd: (category: TransactionCategory) -> Unit = {},
+    onDismiss: () -> Unit = {}
+) {
+    AlertDialog(
+        containerColor = MaterialTheme.colors.background,
+        onDismissRequest = onDismiss,
+        title = {
+            Row(Modifier.fillMaxWidth()) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "",
+                    tint = MaterialTheme.colors.secondary,
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .clickable { onDismiss() }
+                )
+                Text(
+                    text = stringResource(id = R.string.select_category),
+                    color = MaterialTheme.colors.secondary,
+                    fontSize = 18.sp,
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .padding(start = 8.dp)
+                )
+            }
+        },
+        text = {
+            CategorySelector(isIncome, defaultCategory) {
+                onAdd(it)
+                onDismiss()
+            }
+        },
+        confirmButton = {})
+}
+
+@Composable
+private fun CategorySelector(
+    isIncome: Boolean,
+    defaultCategory: TransactionCategory? = null,
+    onAdd: (category: TransactionCategory) -> Unit,
+) {
+    val isShowIncomeCategories = remember { mutableStateOf(isIncome) }
+    val selectedOption = remember {
+        mutableStateOf(defaultCategory)
+    }
+    val items =
+        if (isShowIncomeCategories.value) TransactionCategory.IncomeCategory.getItems() else TransactionCategory.ExpenseCategory.getItems()
+
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.fillMaxWidth()) {
+            Button(
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = if (isShowIncomeCategories.value) blue else MaterialTheme.colors.background,
+                    contentColor = if (isShowIncomeCategories.value) white else MaterialTheme.colors.secondary
+                ), modifier = Modifier.weight(1f),
+                onClick = {isShowIncomeCategories.value = true}
+            ) {
+                Text(text = stringResource(id = R.string.income))
+            }
+            Button(
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = if (!isShowIncomeCategories.value) blue else MaterialTheme.colors.background,
+                    contentColor = if (!isShowIncomeCategories.value) white else MaterialTheme.colors.secondary
+                ), modifier = Modifier.weight(1f),
+                onClick = {isShowIncomeCategories.value = false}
+            ) {
+                Text(text = stringResource(id = R.string.expense))
+            }
+        }
+        LazyColumn {
+            items(items.size) {
+                val item = items[it]
+                Row(
+                    Modifier
+                        .padding(4.dp)
+                        .clickable {
+                            selectedOption.value = item
+                            onAdd(item)
+                        },
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Card(
+                        modifier = Modifier
+                            .size(48.dp)
+                            .padding(4.dp)
+                            .align(Alignment.CenterVertically),
+                        shape = RoundedCornerShape(8.dp),
+                    ) {
+                        Box(modifier = Modifier.background(Color(item.color))) {
+                            androidx.compose.material.Icon(
+                                painter = painterResource(id = item.icon),
+                                contentDescription = "",
+                                Modifier
+                                    .align(Alignment.Center)
+                                    .size(32.dp),
+                                tint = white
+                            )
+                        }
+                    }
+                    Text(
+                        text = stringResource(id = item.description),
+                        color = MaterialTheme.colors.secondary,
+                        modifier = Modifier
+                            .padding(start = 12.dp)
+                            .weight(1f),
+                        fontSize = 18.sp
+                    )
+                    RadioButton(
+                        selected = (item == selectedOption.value),
+                        onClick = null
+                    )
+                }
+            }
+        }
+    }
+}
