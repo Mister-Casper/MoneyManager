9ff86ffaf6b1279fd62a7af385b6d8e66be78364
UserName <44705397+Mister-Casper@users.noreply.github.com>
2022-03-13 11:06:12 +0700

    Update DatePicker.kt

--- app/src/main/java/com/sgcdeveloper/moneymanager/presentation/ui/dialogs/DatePicker.kt
+++ app/src/main/java/com/sgcdeveloper/moneymanager/presentation/ui/dialogs/DatePicker.kt
@@ -1,14 +1,15 @@
 package com.sgcdeveloper.moneymanager.presentation.ui.dialogs
 
+import android.app.DatePickerDialog
+import android.widget.DatePicker
 import androidx.compose.material.AlertDialog
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.window.DialogProperties
-import androidx.fragment.app.FragmentActivity
-import com.google.android.material.datepicker.MaterialDatePicker
 import com.sgcdeveloper.moneymanager.util.Date
 import java.time.LocalDate
+import java.util.*
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
@@ -24,15 +25,18 @@ fun DatePicker(
         onDismissRequest = { onDismissRequest() },
         properties = DialogProperties(usePlatformDefaultWidth = false),
         text = {
-            MaterialDatePicker.Builder.datePicker()
-                .setSelection(defaultDate.epochMillis)
-                .build()
-                .apply {
-                    addOnPositiveButtonClickListener { onDateSelected(Date(it).getAsLocalDate()) }
-                    addOnDismissListener { onDismissRequest() }
-                    show((context as FragmentActivity).supportFragmentManager, "tag")
-                }
-
+            DatePickerDialog(
+                context,
+                { q: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+                    val cal = Calendar.getInstance()
+                    cal.set(year, month, dayOfMonth)
+                    onDateSelected(Date(cal.timeInMillis).getAsLocalDate())
+                    onDismissRequest()
+                },
+                defaultDate.getAsLocalDate().year,
+                defaultDate.getAsLocalDate().monthValue - 1,
+                defaultDate.getAsLocalDate().dayOfMonth
+            ).show()
         },
         buttons = {})
 }
