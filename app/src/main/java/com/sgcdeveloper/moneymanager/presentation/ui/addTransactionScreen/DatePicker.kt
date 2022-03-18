f4d6fedcae719387af2ea7fed8a68b24d6ca6fdd
UserName <44705397+Mister-Casper@users.noreply.github.com>
2022-03-18 18:31:40 +0700

    Update DatePicker.kt

--- app/src/main/java/com/sgcdeveloper/moneymanager/presentation/ui/addTransactionScreen/DatePicker.kt
+++ app/src/main/java/com/sgcdeveloper/moneymanager/presentation/ui/addTransactionScreen/DatePicker.kt
@@ -1,41 +1,48 @@
 package com.sgcdeveloper.moneymanager.presentation.ui.addTransactionScreen
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.sgcdeveloper.moneymanager.R
 
 @Composable
 fun DatePicker(addTransactionViewModel: AddTransactionViewModel) {
+    val source = remember { MutableInteractionSource() }
+
+    if (source.collectIsPressedAsState().value) {
+        addTransactionViewModel.onEvent(AddTransactionEvent.ShowChangeDateDialog)
+    }
+
     Row(Modifier.fillMaxWidth()) {
-        Text(
-            text = stringResource(
+        TextField(
+            value = stringResource(
                 id = R.string.date,
                 addTransactionViewModel.transactionDate.value.toDateString()
-            ), fontSize = 22.sp, modifier = Modifier
-                .weight(1f)
-                .align(Alignment.CenterVertically),
-            color = MaterialTheme.colors.secondary
-        )
-        Icon(
-            painter = painterResource(id = R.drawable.edit_calendar_icon),
-            contentDescription = "",
-            tint = MaterialTheme.colors.secondary,
+            ),
+            onValueChange = {},
+            readOnly = true,
             modifier = Modifier
-                .size(32.dp)
                 .align(Alignment.CenterVertically)
-                .clickable { addTransactionViewModel.onEvent(AddTransactionEvent.ShowChangeDateDialog) }
+                .padding(top = 12.dp, start = 20.dp, end = 20.dp)
+                .fillMaxWidth(),
+            colors = TextFieldDefaults.textFieldColors(textColor = MaterialTheme.colors.secondary),
+            singleLine = true,
+            trailingIcon = {
+                Icon(painter = painterResource(id = R.drawable.edit_calendar_icon), "",Modifier.size(32.dp))
+            }, placeholder = {
+                Text(text = stringResource(id = R.string.select_category))
+            }, interactionSource = source
         )
     }
 }
