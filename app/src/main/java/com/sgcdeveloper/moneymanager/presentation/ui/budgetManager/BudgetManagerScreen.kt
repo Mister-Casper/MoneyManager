6bf14535edbd51acb3e48e77b80b48dec7beb049
UserName <44705397+Mister-Casper@users.noreply.github.com>
2022-03-24 21:07:45 +0700

    Update BudgetManagerScreen.kt

--- app/src/main/java/com/sgcdeveloper/moneymanager/presentation/ui/budgetManager/BudgetManagerScreen.kt
+++ app/src/main/java/com/sgcdeveloper/moneymanager/presentation/ui/budgetManager/BudgetManagerScreen.kt
@@ -3,9 +3,8 @@ package com.sgcdeveloper.moneymanager.presentation.ui.budgetManager
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
@@ -14,6 +13,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -22,53 +22,71 @@ import androidx.navigation.NavController
 import com.sgcdeveloper.moneymanager.R
 import com.sgcdeveloper.moneymanager.domain.model.BaseBudget
 import com.sgcdeveloper.moneymanager.presentation.nav.Screen
+import com.sgcdeveloper.moneymanager.presentation.theme.blue
 import com.sgcdeveloper.moneymanager.presentation.theme.dark_gray
+import com.sgcdeveloper.moneymanager.presentation.theme.white
 import com.sgcdeveloper.moneymanager.presentation.ui.composables.RoundedLinearProgressIndicator
 import com.sgcdeveloper.moneymanager.presentation.ui.homeScreen.HomeViewModel
 
 @Composable
 fun BudgetManagerScreen(homeViewModel: HomeViewModel, navController: NavController) {
     val budgets = remember { homeViewModel.budgets }
-    Column(Modifier.fillMaxWidth()) {
-        Box(modifier = Modifier.fillMaxWidth()) {
-            Row(Modifier.align(Alignment.CenterStart)) {
-                Icon(
-                    imageVector = Icons.Filled.ArrowBack,
-                    contentDescription = "",
-                    tint = MaterialTheme.colors.secondary,
-                    modifier = Modifier
-                        .align(Alignment.CenterVertically)
-                        .size(40.dp)
-                        .clickable { navController.popBackStack() }
-                )
-                Text(
-                    text = stringResource(id = R.string.budget_view),
-                    color = MaterialTheme.colors.secondary,
-                    fontSize = 24.sp,
-                    modifier = Modifier
-                        .align(Alignment.CenterVertically)
-                        .padding(start = 8.dp)
-                        .weight(1f)
-                )
-            }
-        }
-        LazyColumn(
-            Modifier
-                .fillMaxSize()
-                .padding(12.dp)
-        ) {
-            items(budgets.size) {
-                val budget = budgets[it]
-                if (budget is BaseBudget.BudgetHeader) {
-                    BudgetHeader(budget) {
-
-                    }
-                } else if (budget is BaseBudget.BudgetItem) {
-                    BudgetItem(budget) {
-                        navController.navigate(Screen.BudgetScreen(budget).route)
-                    }
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(Modifier.fillMaxSize()) {
+            Box(modifier = Modifier.fillMaxWidth()) {
+                Row(Modifier.align(Alignment.CenterStart)) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "",
+                        tint = MaterialTheme.colors.secondary,
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically)
+                            .size(40.dp)
+                            .clickable { navController.popBackStack() }
+                    )
+                    Text(
+                        text = stringResource(id = R.string.budget_view),
+                        color = MaterialTheme.colors.secondary,
+                        fontSize = 24.sp,
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically)
+                            .padding(start = 8.dp)
+                            .weight(1f)
+                    )
                 }
             }
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(budgets.size) {
+                    val budget = budgets[it]
+                    if (budget is BaseBudget.BudgetHeader) {
+                        BudgetHeader(budget) {
+
+                        }
+                    } else if (budget is BaseBudget.BudgetItem) {
+                        BudgetItem(budget) {
+                            navController.navigate(Screen.BudgetScreen(budget).route)
+                        }
+                    }
+                }
+
+                item {
+                    Spacer(modifier = Modifier.padding(top = 64.dp))
+                }
+            }
+        }
+        OutlinedButton(
+            onClick = { navController.navigate(Screen.AddBudgetScreen().route) },
+            shape = CircleShape,
+            modifier = Modifier.align(Alignment.BottomCenter).padding(bottom = 8.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = blue)
+        ) {
+
+            androidx.compose.material.Icon(
+                painter = painterResource(id = R.drawable.add_icon),
+                contentDescription = "",
+                tint = white
+            )
+            Text(text = stringResource(id = R.string.add_budget), color = white)
         }
     }
 }
