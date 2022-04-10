6fde86d97afb8c80280a3b7af524c7da9dae2952
UserName <44705397+Mister-Casper@users.noreply.github.com>
2022-04-10 19:51:25 +0700

    Update WalletPickerDialog.kt

--- app/src/main/java/com/sgcdeveloper/moneymanager/presentation/ui/dialogs/WalletPickerDialog.kt
+++ app/src/main/java/com/sgcdeveloper/moneymanager/presentation/ui/dialogs/WalletPickerDialog.kt
@@ -1,6 +1,5 @@
 package com.sgcdeveloper.moneymanager.presentation.ui.dialogs
 
-import android.content.Context
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -103,7 +102,6 @@ private fun WalletSelector(
     onAdd: (wallet: Wallet) -> Unit,
     onAddNewWallet: (newWallet: Wallet) -> Unit = {}
 ) {
-    val context = LocalContext.current
     var selectedOption by remember {
         mutableStateOf(defaultWallet)
     }
@@ -116,7 +114,7 @@ private fun WalletSelector(
                         onAddNewWallet(item)
                     }
                 } else {
-                    ExistWalletItem(context,item, selectedOption) {
+                    ExistWalletItem(item, selectedOption) {
                         selectedOption = item
                         onAdd(item)
                     }
@@ -127,11 +125,7 @@ private fun WalletSelector(
 }
 
 @Composable
-<<<<<<< HEAD
-fun ExistWalletItem(context:Context,item: Wallet, selectedItem: Wallet?, onClick: () -> Unit) {
-=======
 fun ExistWalletItem(item: Wallet, selectedItem: Wallet?, onClick: () -> Unit) {
->>>>>>> parent of 480ad14 (Fixed bug)
     Row(
         Modifier
             .padding(4.dp)
